37f7185a47360143401b5937f606d5bbc419c43f
Grant <gdawson2018@gmail.com>
2021-01-21 14:54:04 -0500

    Adding small class

---
+++ StockData.py
@@ -0,0 +1,17 @@
+import math
+import numpy as np
+
+
+class Stock:
+
+    def __init__(self):
+        self.ticker = "NONE"        #Ticker of the stock
+        self.price = 0.0            #Price of the stock
+        self.precentChange = 0.0    #Daily precent chanage
+        self.change = 0.0           #Dollar change value
+        self.precentDividend = 0.0  #Dividend precent
+        self.dividend = 0.0         #Dividend dollar value
+
+
+
+
