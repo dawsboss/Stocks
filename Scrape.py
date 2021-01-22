366c413538c7159bcc0a008d2606c795a94e2b51
dawsboss <gdawson2018@gmail.com>
2021-01-22 15:09:26 -0500

    Adding more to the stock class and learning more about scraping

--- Scrape.py
+++ Scrape.py
@@ -1,15 +1,30 @@
+import pandas
 import requests
-
 from bs4 import BeautifulSoup as bs
+import analysis_engine.finviz.fetch_api as fv
 
-webpage = requests.get("https://linuxhint.com/python-web-scraping-tutorial/")
+print("test")
 
+webpage = requests.get("https://www.dividendinvestor.com/projected-dividend-yield/?symbol=cldt&proj_div_yield=10")
 webcontent = webpage.content
+#htmlcontent = bs(webcontent, "html.parser")
+#for title in htmlcontent.find_all('h1'):
+#    print(title)
 
-htmlcontent = bs(webcontent, "html.parser")
 
-for title in htmlcontent.find_all('p'):
-    print(title)
+url = (
+        'https://finviz.com/screener.ashx?'
+        'v=111&'
+        'f=cap_midunder,exch_nyse,fa_div_o5,idx_sp500'
+        '&ft=4')
+res = fv.fetch_tickers_from_screener(url=url)
+print(res)
+
+print("End")
+
+
+
+
 
 #html = open('test1.html').read()
 
--- StockData.py
+++ StockData.py
@@ -3,14 +3,49 @@ import numpy as np
 
 
 class Stock:
-
     def __init__(self):
-        self.ticker = "NONE"        #Ticker of the stock
-        self.price = 0.0            #Price of the stock
-        self.precentChange = 0.0    #Daily precent chanage
-        self.change = 0.0           #Dollar change value
-        self.precentDividend = 0.0  #Dividend precent
-        self.dividend = 0.0         #Dividend dollar value
+        self.ticker = "NONE"            #Ticker of the stock
+        self.name = "NONE"              #Name of the company 
+        self.numOfShares = 0            #Number of shares you own
+        self.buyingPrice = 0            #(Avg) Price stocks bought
+        self.price = 0.0                #Price of the stock
+        self.precentChange = 0.0        #Daily precent chanage
+        self.change = 0.0               #Dollar change value
+        self.todayTotalChangePrice = 0  #Change * ShareNum
+        self.PE = 0                     #P/E ratio
+        self.costBase = 0               #numShares * BuyingPrice
+        self.marketValue = 0            #Price * numShares
+        self.gainsLosses = 0            #MarketValue - CostBase
+        self.growth = 0                 #total growth
+        self.dividend = 0.0             #Dividend dollar value
+        self.precentDividend = 0.0      #Dividend precent
+        self.payoutType = "NONE"        #How often dividends payout
+        self.nextPayoutData = "Never"   #Date of the next payout
+    
+    def updatePrice(self):
+        pass
+    def updatePrecentChange(self):
+        pass
+    def updateChange(self):
+        pass
+    def updatePE(self):
+        pass
+    def updateCostBase(self):
+        self.costBase = self.numOfShares * self.buyingPrice
+    def updateMarketValue(self):
+        slef.marketValue = self.price * self.nomOfShares
+    def updateGainsLosses(self):
+        self.gainsLosses = self.marketValue - self.costBase
+    def updateGrowth(self):
+        self.growth = (self.price - self.buyingPrice)/self.buyingPrice
+    def updateDividend(self):
+        pass
+    def updatePrecentDividend(self):
+        pass
+    def updatePayoutType(self):
+        pass
+    def updateNextPayoutDate(Self):
+        pass
 
 
 
