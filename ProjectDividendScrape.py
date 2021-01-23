606112709a86266c16b412716f48e7b45d9390c6
dawsboss <gdawson2018@gmail.com>
2021-01-22 22:43:45 -0500

    Prototype for scrapping done. Started on next websites prototype

--- ProjectDividendScrape.py
+++ ProjectDividendScrape.py
@@ -5,12 +5,12 @@ from bs4 import BeautifulSoup as bs
 print("test")
 
 
-url = 'https://finviz.com/quote.ashx?t=REML'
+url = 'https://www.dividendinvestor.com/projected-dividend-yield/?symbol=cldt&proj_div_yield=10'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 getURL = requests.get(url, headers=headers)
 website = bs(getURL.text, 'html.parser')
-html = website.find_all('table')[4]
+html = website.find_all('table')[3]
 
 
 print(html)
